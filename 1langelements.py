4aeb0c3bc2e3af08d65b02a0471abdecd726ae8c
cybersimha <cybersimha@outlook.com>
2023-07-09 23:24:35 +0530

    file create and initial commit

---
+++ 1langelements.py
@@ -0,0 +1,23 @@
+#########################################################
+#   by Narasimha Rao Dattappa                           #
+# I will cover basic language elements in this one file #
+# I will Cover the following                            #
+# pre-requisites: Python should be already available    #
+# to run: python <filename.py>                          #
+# 1.Variable Declaration (major data types)             #
+# 2.Arrays                                              #
+# 3.Lists                                               #
+# 4.Conditional Constructs                              #
+# 5.Looping Constructs                                  #
+#########################################################
+
+
+#Declare variables
+#Declare a numeric variable, variables 
+counter=0
+
+print(counter)
+
+counter = counter+1
+
+print(counter)
